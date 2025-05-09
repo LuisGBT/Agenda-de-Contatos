36149d366cdd61f0d404806df174f77e788c8fe0
LuisGBT <luizingustavo507@gmail.com>
2025-05-08 21:54:44 -0300

    error

---
+++ src/agenda/EventClick.java
@@ -0,0 +1,11 @@
+package agenda;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class EventClick implements ActionListener {
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
---
+++ src/agenda/PainelCriarArquivo.java
@@ -0,0 +1,14 @@
+package agenda;
+
+import javax.swing.*;
+import java.awt.event.ActionListener;
+
+public class PainelCriarArquivo extends JPanel {
+    private JFrame parentFrame;
+
+    public PainelCriarArquivo(JFrame parentFrame){
+    this.parentFrame = parentFrame;
+    parentFrame.setSize(550,550);
+    }
+    
+}
